808f90750c48297c1b74a39294ddf162c8c3bdc9
Jinwook Jung <jeenuk.chung@gmail.com>
2024-12-16 18:55:11 +0900

    add swagger and simple test

---
+++ src/utils/swaggerConfig.js
@@ -0,0 +1,24 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const path = require('path');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Chat Service API',
+            version: '1.0.0',
+            description: 'Chat Service API Documentation',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+                description: 'Local Server',
+            },
+        ],
+    },
+    apis: [path.join(__dirname, '../server.js')],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
