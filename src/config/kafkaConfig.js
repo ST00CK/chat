2f0532a1923e71feb1c6c9aaadc02cab773db2e9
Jinwook Jung <jeenuk.chung@gmail.com>
2025-01-12 13:52:24 +0900

    Kafka Client ID 설정

--- src/config/kafkaConfig.js
+++ src/config/kafkaConfig.js
@@ -1,11 +1,14 @@
+require ('dotenv').config({ path: '../.env' }); // .env 파일 로드
 const { Kafka } = require('kafkajs');
-require ('dotenv').config(); // .env 파일 로드
 
 const kafkaBrokers = [process.env.KAFKA_BROKERS];
 
-const kafka = new Kafka({
-    clientId: '',
-    brokers: kafkaBrokers,
-});
+// Kafka 객체 생성 함수 ( 로그와 모니터링을 쉽게하기 위해 clientID 를 Consumer 와 Producer 에서 나누어서 관리하기 )
+const createKafkaInstance = (clientId) => {
+    return new Kafka({
+        clientId: clientId, // 역할별 clientId 지정
+        brokers: kafkaBrokers,
+    });
+};
 
-module.exports = kafka;
+module.exports = createKafkaInstance;
