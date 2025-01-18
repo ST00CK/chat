2b8b047755f9350fa8c4dfea99bb4af138e0edad
Jinwook Jung <jeenuk.chung@gmail.com>
2025-01-18 16:11:46 +0900

    ScyllaDB 에 Query 로 데이터 가지고 올 파일 정의

---
+++ src/services/CassandraDataQuery.js
@@ -0,0 +1,16 @@
+const client = require('../config/scyllaDBConfig');
+const { Uuid } = require('cassandra-driver').types;
+
+async function getUsersInRoom(roomId) {
+    const query = `SELECT user_id FROM chat_messages WHERE room_id = ?`;
+    try{
+        const normalizedRoomId = typeof roomId === 'string' ? Uuid.fromString(roomId) : roomId ;
+        const result = await client.execute(query, [normalizedRoomId]);
+        return result.rows.map(row => row.user_id); // 유저 ID 목록 반환
+    } catch (err) {
+        console.error('Error in CassandraDataQuery_getUsersInRoom: ', err);
+        return [];
+    }
+}
+
+module.exports = { getUsersInRoom };
