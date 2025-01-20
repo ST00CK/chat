abceaf07a3a73bf364a4cbda94edc6836595d678
jeenukchung <jeenuk.chung@gmail.com>
2025-01-20 15:35:08 +0900

    SycllaDB 쿼리 조회 오류 수정중

--- src/services/CassandraDataQuery.js
+++ src/services/CassandraDataQuery.js
@@ -2,9 +2,10 @@ const client = require('../config/scyllaDBConfig');
 const { Uuid } = require('cassandra-driver').types;
 
 async function getUsersInRoom(roomId) {
+    console.log("-0-0-0-0-0-0-0-", typeof roomId);
     const query = `SELECT user_id FROM chat_messages WHERE room_id = ?`;
     try{
-        const normalizedRoomId = typeof roomId === 'string' ? Uuid.fromString(roomId) : roomId ;
+        const normalizedRoomId = typeof roomId === 'string' ? roomId : roomId ;
         const result = await client.execute(query, [normalizedRoomId]);
         return result.rows.map(row => row.user_id); // 유저 ID 목록 반환
     } catch (err) {
